b0df9a817d7e878d1619f585fe80a9d9c322dd61
atravers07 <36854393+atravers07@users.noreply.github.com>
2018-02-26 22:15:15 +0100

    Update partA.py

--- partA.py
+++ partA.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 
 def main():
-	f=open("text.txt","r")
+	#f=open("text.txt","r")
 	my_dictionnary={}
 	ponctuation_list=[",","?",";",".",":","/","!","'","(",")","[","]"]
+	with open('text.txt','r') as my_file:
+		contenu=my_file.read()
+		print(contenu)
 	for char in ponctuation_list:
-		f=f.replace(char,' ')
-	f=f.lover()
-	for line in f:
-		words=line.split()
-		for word in words:
+		contenu2=my_file.replace(char,' ')
+		print(contenu2)
+	#f=f.lover()
+	#	for line in my_file:
+	#		words=line.split()
+		for word in contenu:
 			if word in my_dictionnary:
 				my_dictionnary[word]+=1
 			else:
 				my_dictionnary[word]=1	
-	for cle in my_dictionnary:
-		print(cle)
+		for cle in my_dictionnary:
+			print(cle)
 	
 
 if __name__=="__main__":
 	main()
+
